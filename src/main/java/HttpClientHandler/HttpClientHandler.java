5db3cfa8fe64702bc9ee42c4765fbf165afcd061
AmirmahdiGolahmar <amirmahdigolahmar@gmail.com>
2025-07-17 23:17:19 +0330

    Add HttpClientHandler

---
+++ src/main/java/HttpClientHandler/HttpClientHandler.java
@@ -0,0 +1,31 @@
+package HttpClientHandler;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpClientHandler {
+    public static String sendPostRequest(String urlStr, String jsonInputString) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                response.append(line.trim());
+            }
+            return response.toString();
+        }
+    }
+}
