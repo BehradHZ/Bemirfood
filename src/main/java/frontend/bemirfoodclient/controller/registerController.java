8542a04794be1b20747124e0a49c54e34df4ad12
AmirmahdiGolahmar <amirmahdigolahmar@gmail.com>
2025-07-18 02:44:51 +0330

    Connect Register to back

--- src/main/java/frontend/bemirfoodclient/controller/registerController.java
+++ src/main/java/frontend/bemirfoodclient/controller/registerController.java
@@ -1,10 +1,8 @@
 package frontend.bemirfoodclient.controller;
 
-import HttpClientHandler.HttpClientHandler;
 import HttpClientHandler.HttpResponseData;
+import HttpClientHandler.HttpClientHandler;
 import frontend.bemirfoodclient.BemirfoodApplication;
-import javafx.animation.PauseTransition;
-import javafx.application.Platform;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -16,7 +14,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -64,6 +61,7 @@ public class registerController {
 
     @FXML
     public void initialize() {
+
         firstPageImage.fitHeightProperty().bind(mainBorderPane.heightProperty());
 
         UnaryOperator<TextFormatter.Change> phoneNumberFilter = change -> {
@@ -85,13 +83,6 @@ public class registerController {
         visiblePasswordField.visibleProperty().bind(makePasswordHidden.visibleProperty());
         passwordField.visibleProperty().bind(makePasswordVisible.visibleProperty());
 
-        roleToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-           if(newValue != null) {
-               RadioButton selectedRadioButton = (RadioButton) newValue;
-               role = selectedRadioButton.getText().toLowerCase();
-           }
-        });
-
         BooleanBinding allFieldsFilled =
                 fullNameTextField.textProperty().isEmpty().or(
                         phoneNumberTextField.textProperty().isEmpty().or(
@@ -108,17 +99,44 @@ public class registerController {
         registerButton.disableProperty().bind(allFieldsFilled.or(isRegistering));
 
         //temporary
-        fullNameTextField.setText("09");
-        phoneNumberTextField.setText("09");
-        emailTextField.setText("09");
-        passwordField.setText("09");
-        bankNameTextField.setText("09");
-        accountNumberTextField.setText("09");
+        fullNameTextField.setText("");
+        phoneNumberTextField.setText("");
+        emailTextField.setText("");
+        passwordField.setText("");
+        bankNameTextField.setText("");
+        accountNumberTextField.setText("");
     }
 
     public int checkLoginStatus(String fullName, String phoneNumber, String email, String password, String role, String bankName, String accountNumber) {
         //do the stuff in backend
-        return 200; //temporary
+        String json = String.format("""
+        {
+          "password": "%s",
+          "full_name": "%s",
+          "mobile": "%s",
+          "email": "%s",
+          "address": "",
+          "profileImageBase64": "",
+          "bank_info": {
+            "bank_name": "%s",
+            "account_number": "%s"
+          },
+          "role": "%s"
+        }
+        """,
+                    password, fullName, phoneNumber, email,
+                    bankName, accountNumber, role
+            );
+
+        int code = 0;
+        try {
+            HttpResponseData responseData = HttpClientHandler.sendPostRequest("http://localhost:4567/auth/register", json);
+            code = responseData.getStatusCode();
+            String body = responseData.getBody();
+            return code;
+        } catch (IOException e) {
+            return 500;
+        }
     }
 
     public String checkLoginRole(String phoneNumber){
@@ -141,95 +159,123 @@ public class registerController {
         };
     }
 
-
-
     @FXML
     public void handelRegisterButtonClicked() {
+        RadioButton selectedToggle = (RadioButton) roleToggleGroup.getSelectedToggle();
+        role = ((RadioButton) selectedToggle).getText().toLowerCase();
         isRegistering.set(true);
-
-        String fullName = fullNameTextField.getText();
-        String mobile = phoneNumberTextField.getText();
-        String email = emailTextField.getText();
-        String password = passwordField.getText();
-        String bankName = bankNameTextField.getText();
-        String accountNumber = accountNumberTextField.getText();
-
-        String json = String.format("""
-    {
-      "password": "%s",
-      "full_name": "%s",
-      "mobile": "%s",
-      "email": "%s",
-      "address": "",
-      "profileImageBase64": "",
-      "bank_info": {
-        "bank_name": "%s",
-        "account_number": "%s"
-      },
-      "role": "%s"
-    }
-    """,
-                password, fullName, mobile, email,
-                bankName, accountNumber, role
-        );
-
-        new Thread(() -> {
-            int code = 0;
-            try {
-                HttpResponseData responseData = HttpClientHandler.sendPostRequest("http://localhost:4567/auth/register", json);
-                code = responseData.getStatusCode();
-                String body = responseData.getBody();
-            } catch (IOException e) {
-                showErrorAlertWithIcon("This is from our side; please try again later :) (500 Internal Server Error)");
-            }
-
-            int finalCode = code;
-            Platform.runLater(() -> {
-                switch (finalCode) {
-                    case 200 -> {
-                        PauseTransition delay = new PauseTransition(Duration.seconds(5));
-                        delay.setOnFinished(event -> {
-                            try {
-                                Stage stage = (Stage) phoneNumberTextField.getScene().getWindow();
-                                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
-                                        checkLoginRole(phoneNumberTextField.getText()))));
-                                stage.getScene().setRoot(root);
-                            } catch (IOException ioe) {
-                                ioe.printStackTrace();
-                            }
-                        });
-                        delay.play();
-                    }
-                    case 400 -> showErrorAlertWithIcon("Invalid phone number or password. (400 Invalid input)");
-                    case 401 -> showErrorAlertWithIcon("This phone number is not registered. (401 Unauthorized)");
-                    case 403 -> showErrorAlertWithIcon("You cannot access this service. (403 Forbidden)");
-                    case 404 -> showErrorAlertWithIcon("Service not found. (404 Not Found)");
-                    case 409 -> showErrorAlertWithIcon("This phone number already exists. (409 Conflict)");
-                    case 415 -> showErrorAlertWithIcon("This media type cannot be accepted. (415 Unsupported Media Type)");
-                    case 429 -> showErrorAlertWithIcon("Please try again later. (429 Too Many Requests)");
-                    case 500 -> showErrorAlertWithIcon("This is from our side; please try again later :) (500 Internal Server Error)");
-                    default -> {
-                        registerButton.setDisable(false);
-                        showErrorAlertWithIcon("Unexpected response code: " + finalCode);
-                    }
+        switch (checkLoginStatus(fullNameTextField.getText(),
+                phoneNumberTextField.getText(),
+                emailTextField.getText(),
+                passwordField.getText(),
+                role,
+                bankNameTextField.getText(),
+                accountNumberTextField.getText())){
+            case 200:
+                try {
+                    Stage stage = (Stage) phoneNumberTextField.getScene().getWindow();
+                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
+                            checkLoginRole(phoneNumberTextField.getText()))));
+                    stage.getScene().setRoot(root);
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
                 }
-            });
-        }).start();
+
+                break;
+            case 400:
+                isRegistering.set(false);
+                Alert alert400 = new Alert(Alert.AlertType.ERROR);
+                Stage stage400 = (Stage) alert400.getDialogPane().getScene().getWindow();
+                stage400.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert400.setTitle("Login failed");
+                alert400.setHeaderText(null);
+                alert400.setContentText("Invalid phone number or password. (400 Invalid input)");
+                alert400.getDialogPane().setGraphic(null);
+                alert400.showAndWait();
+                break;
+            case 401:
+                isRegistering.set(false);
+                Alert alert401 = new Alert(Alert.AlertType.ERROR);
+                Stage stage401 = (Stage) alert401.getDialogPane().getScene().getWindow();
+                stage401.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert401.setTitle("Login failed");
+                alert401.setHeaderText(null);
+                alert401.setContentText("This phone number is not registered. (401 Unauthorized)");
+                alert401.getDialogPane().setGraphic(null);
+                alert401.showAndWait();
+                break;
+            case 403:
+                isRegistering.set(false);
+                Alert alert403 = new Alert(Alert.AlertType.ERROR);
+                Stage stage403 = (Stage) alert403.getDialogPane().getScene().getWindow();
+                stage403.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert403.setTitle("Login failed");
+                alert403.setHeaderText(null);
+                alert403.setContentText("You cannot access to this service. (403 Forbidden)");
+                alert403.getDialogPane().setGraphic(null);
+                alert403.showAndWait();
+                break;
+            case 404:
+                isRegistering.set(false);
+                Alert alert404 = new Alert(Alert.AlertType.ERROR);
+                Stage stage404 = (Stage) alert404.getDialogPane().getScene().getWindow();
+                stage404.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert404.setTitle("Login failed");
+                alert404.setHeaderText(null);
+                alert404.setContentText("Service not found. (404 Not Found)");
+                alert404.getDialogPane().setGraphic(null);
+                alert404.showAndWait();
+                break;
+            case 409:
+                isRegistering.set(false);
+                Alert alert409 = new Alert(Alert.AlertType.ERROR);
+                Stage stage409 = (Stage) alert409.getDialogPane().getScene().getWindow();
+                stage409.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert409.setTitle("Login failed");
+                alert409.setHeaderText(null);
+                alert409.setContentText("This phone number already exist. (409 Phone number already exist)");
+                alert409.getDialogPane().setGraphic(null);
+                alert409.showAndWait();
+                break;
+            case 415:
+                isRegistering.set(false);
+                Alert alert415 = new Alert(Alert.AlertType.ERROR);
+                Stage stage415 = (Stage) alert415.getDialogPane().getScene().getWindow();
+                stage415.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert415.setTitle("Login failed");
+                alert415.setHeaderText(null);
+                alert415.setContentText("This media type cannot be accepted. (415 Unsupported Media Type)");
+                alert415.getDialogPane().setGraphic(null);
+                alert415.showAndWait();
+                break;
+            case 429:
+                isRegistering.set(false);
+                Alert alert429 = new Alert(Alert.AlertType.ERROR);
+                Stage stage429 = (Stage) alert429.getDialogPane().getScene().getWindow();
+                stage429.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert429.setTitle("Login failed");
+                alert429.setHeaderText(null);
+                alert429.setContentText("Please try again later. (429 Too Many Requests)");
+                alert429.getDialogPane().setGraphic(null);
+                alert429.showAndWait();
+                break;
+            case 500:
+                isRegistering.set(false);
+                Alert alert500 = new Alert(Alert.AlertType.ERROR);
+                Stage stage500 = (Stage) alert500.getDialogPane().getScene().getWindow();
+                stage500.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
+                alert500.setTitle("Login failed");
+                alert500.setHeaderText(null);
+                alert500.setContentText("This is from our side; pleas try again later :) (500 Internal Server Error)");
+                alert500.getDialogPane().setGraphic(null);
+                alert500.showAndWait();
+                break;
+            default:
+                isRegistering.set(false);
+                break;
+        }
     }
 
-
-    private void showErrorAlertWithIcon(String message) {
-        registerButton.setDisable(false);
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-        stage.getIcons().add(new Image(Objects.requireNonNull(
-                BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
-        alert.setTitle("Registration failed");
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.getDialogPane().setGraphic(null);
-        alert.showAndWait();
-    }
 
 
 
