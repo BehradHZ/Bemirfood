e746cc0e1a0f67af3bf66b8e217ffef5d469f7c4
BehradHZ <bhvsrt2006@gmail.com>
2025-07-24 22:35:10 +0330

    random commit

--- src/main/java/frontend/bemirfoodclient/controller/profile/courier/details/CourierMyOrdersController.java
+++ src/main/java/frontend/bemirfoodclient/controller/profile/courier/details/CourierMyOrdersController.java
@@ -1,137 +1,43 @@
-package frontend.bemirfoodclient.controller.border;
+package frontend.bemirfoodclient.controller.profile.courier.details;
 
 import frontend.bemirfoodclient.BemirfoodApplication;
 import frontend.bemirfoodclient.controller.restaurant.courier.DeliveryCardController;
 import frontend.bemirfoodclient.model.entity.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class CourierBorderController {
-    @FXML
-    public ImageView borderBemirfoodLogo;
-    @FXML
-    public TextField searchTextField;
-    @FXML
-    public ImageView searchIcon;
-    @FXML
-    public ImageView profileIcon;
-    @FXML
-    public Region toolbarSpacer;
+public class CourierMyOrdersController {
     @FXML
     public VBox activeDeliverySection;
     @FXML
-    public VBox recommendedDeliveryList;
+    public ScrollPane activeScroll;
     @FXML
     public Separator noActiveOrder;
     @FXML
+    public ScrollPane recommendedScroll;
+    @FXML
+    public VBox recommendedDeliveryList;
+    @FXML
     public Separator noDeliveryOrder;
     @FXML
-    public ScrollPane activeScroll;
+    public ScrollPane historyScroll;
     @FXML
-    public ScrollPane recommendedScroll;
+    public VBox historyDeliveryList;
+    @FXML
+    public Separator noHistoryOrder;
 
     public void initialize() {
-        borderBemirfoodLogo.setPreserveRatio(true);
-        borderBemirfoodLogo.setFitHeight(40);
-
-        searchIcon.setPreserveRatio(true);
-        searchIcon.setFitHeight(17);
-
-        HBox.setHgrow(toolbarSpacer, Priority.ALWAYS);
-
-        profileIcon.setPreserveRatio(true);
-        profileIcon.setFitHeight(27);
-
         showAllDeliveries();
-    }
-
-    @FXML
-    public void borderBemirfoodLogoClicked() {
-        try {
-            Stage stage = (Stage) profileIcon.getScene().getWindow();
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
-                    "/frontend/bemirfoodclient/border/courier-border-view.fxml")));
-            stage.getScene().setRoot(root);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public int getCurrentUserProfile() {
-        //do the stuff in backend
-        return 200; //temporary
-    }
-
-    @FXML
-    public void profileButtonClicked() {
-        switch (getCurrentUserProfile()){
-            case 200:
-                try {
-                    Stage stage = (Stage) profileIcon.getScene().getWindow();
-                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
-                            "/frontend/bemirfoodclient/profile/courier/courier-profile-view.fxml")));
-                    stage.getScene().setRoot(root);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 400:
-                showAlert("Invalid phone number or password. (400 Invalid input)");
-                break;
-            case 401:
-                showAlert("This phone number is not registered. (401 Unauthorized)");
-                break;
-            case 403:
-                showAlert("You cannot access to this service. (403 Forbidden)");
-                break;
-            case 404:
-                showAlert("Service not found. (404 Not Found)");
-                break;
-            case 409:
-                showAlert("There was a conflict for access to this service. (409 Conflict)");
-                break;
-            case 415:
-                showAlert("This media type cannot be accepted. (415 Unsupported Media Type)");
-                break;
-            case 429:
-                showAlert("Please try again later. (429 Too Many Requests)");
-                break;
-            case 500:
-                showAlert("This is from our side; pleas try again later :) (500 Internal Server Error)");
-            default:
-                break;
-        }
-    }
-
-    public void showAlert(String content) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-        stage.getIcons().add(new Image(Objects.requireNonNull(BemirfoodApplication.class.getResourceAsStream("assets/icons/error.png"))));
-        alert.setTitle("Loading profile failed");
-        alert.setHeaderText(null);
-        alert.setContentText(content);
-        alert.getDialogPane().setGraphic(null);
-        alert.showAndWait();
-    }
-
-    @FXML
-    public void searchButtonClicked() {
-
     }
 
     public List<Order> getAllDeliveries() {
@@ -179,7 +85,7 @@ public class CourierBorderController {
         List<CartItem> order1Items = List.of(new CartItem(pepperoniPizza, 1), new CartItem(veggiePizza, 1));
         Order order1 = new Order(order1Items, customer1.getAddress(), customer1, pizzaPalace,
                 LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(1),
-                fixedDiscount, OrderStatus.on_the_way, 15000.0);
+                fixedDiscount, OrderStatus.completed, 15000.0);
         order1.setDelivery(delivery1); // Manually set the delivery person
         orders.add(order1);
 
@@ -213,8 +119,11 @@ public class CourierBorderController {
                 .filter(order -> order.getStatus() == OrderStatus.finding_courier)
                 .toList();
 
-        activeDeliverySection.getChildren().clear();
+        List<Order> historyList = allOrders.stream()
+                .filter(o -> o.getStatus() == OrderStatus.completed || o.getStatus() == OrderStatus.cancelled)
+                .collect(Collectors.toList());
 
+        activeDeliverySection.getChildren().clear();
         if (activeDeliveries.isEmpty()) {
             activeScroll.setPrefHeight(30);
             noActiveOrder.setVisible(true);
@@ -226,7 +135,6 @@ public class CourierBorderController {
         }
 
         recommendedDeliveryList.getChildren().clear();
-
         if (recommendedDeliveries.isEmpty()) {
             recommendedScroll.setPrefHeight(30);
             noDeliveryOrder.setVisible(true);
@@ -234,6 +142,17 @@ public class CourierBorderController {
             noDeliveryOrder.setVisible(false);
             for (Order order : recommendedDeliveries) {
                 recommendedDeliveryList.getChildren().add(createDeliveryCard(order));
+            }
+        }
+
+        historyDeliveryList.getChildren().clear();
+        if (historyList.isEmpty()) {
+            historyScroll.setPrefHeight(30);
+            noHistoryOrder.setVisible(true);
+        } else {
+            noHistoryOrder.setVisible(false);
+            for (Order order : historyList) {
+                historyDeliveryList.getChildren().add(createDeliveryCard(order));
             }
         }
     }
